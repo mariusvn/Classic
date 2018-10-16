e24791fb7d799308a484d3862fbd2daff9a7da1c
Marius Van Nieuwenhuyse <mariusvnspam@gmail.com>
2018-10-16 22:13:18 +0200

    readline

--- reader.js
+++ reader.js
@@ -6,14 +6,23 @@ const pattern = require("pattern"),
  * @param {String} filepath
  * @return {String}
  */
-function read(filepath) {
+function readFile(filepath) {
     let file;
     try {
         file = fs.readFileSync(filepath).toString();
     } catch (e) {
         throw `Error while reading the file "${filepath}"`;
     }
-    let lines = file.split('\n');
+    return read(file);
+}
+
+/**
+ * read a string and return html string
+ * @param {String} str
+ * @return {String}
+ */
+function read(str) {
+    let lines = str.split('\n');
     let newLines = [];
     for (let i = 0, len = lines.length; i < len; i++)
         newLines.push(pattern.executeLine(lines[i]));
@@ -22,5 +31,6 @@ function read(filepath) {
 
 module.exports = {
     read,
+    readFile,
     createLinePattern: pattern.createLinePattern
 };
